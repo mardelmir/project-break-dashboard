195721ff6fcddfa4a91e4344978666aa4324db2a
María Delgado <mariad1996@gmail.com>
2023-12-09 20:00:23 +0100

    think about good naming in background pictures

--- js/background.js
+++ js/background.js
@@ -1,6 +1,10 @@
+const array = ["posición 0", "posición 1", "posición 2", "posición 3", "posición 4", "posición 5", "posición 6", "posición 7", "posición 8", "posición 9"]
+
 const background = () => {
-    const currentBackground = Math.floor(Math.random() * (10 - 1) + 1)
-    console.log(document.body.style.backgroundColor = `url('../assets/img/${currentBackground}.jpg')`)
+    const currentBackground = Math.floor(Math.random() * (11 - 1) + 1)
+    document.body.style.backgroundImage = `url('../assets/img/${currentBackground}.jpg')`
+    console.log(array[currentBackground])
 }
 
 background()
+setInterval(background, 15000)
