eed2508d5d33a77f03945a507e0152e93d732e56
María Delgado <mariad1996@gmail.com>
2023-12-19 19:44:17 +0100

    implemented weather search bar

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,8 @@
 const baseUrl = 'https://api.weatherapi.com/v1'
 const apiKey = '133633a870f34925876162254231412'
 const city = 'Sevilla'
+const searchLocation = document.getElementById('search-location')
+const results = document.getElementById('search-location-results')
 
 const getWeather = async (url) => {
     try {
@@ -11,32 +13,36 @@ const getWeather = async (url) => {
         const data = await response.json()
         const { condition, cloud, humidity, temp_c: temp, wind_kph: wind } = data.current
         const { text, icon } = condition
-        const currentWeather = { text, icon, cloud, humidity, temp, wind }
+        const weather = { text, icon, cloud, humidity, temp, wind }
+        const location = { name: data.location.name, country: data.location.country }
         const forecast = data.forecast.forecastday[0].hour
 
-        printCurrentWeather(currentWeather)
+        printCurrentWeather(weather, location)
         printHourForecast(forecast)
 
     } catch (error) { console.log('Error al obtener los datos', error) }
 }
 
-const printCurrentWeather = (current) => {
+const printCurrentWeather = (weather, location) => {
+    searchLocation.value = ''
+    results.innerHTML = ''
+    results.classList.add('hidden')
     const locationDiv = document.getElementById('weatherLocation')
     const currentDiv = document.getElementById('currentWeather')
 
     locationDiv.innerHTML = `
-        <p>Sevilla/Andalucía</p>
-        <p class="important">${current.text}</p>`
+    <p>${location.name}/${location.country}</p>
+    <p class="important">${weather.text}</p>`
 
     currentDiv.innerHTML = `
-        <img src="https:${current.icon}" alt="${current.text}" />
-        <p>${current.temp} ºC</p>
-        <img src="../assets/img/degree.png" alt="degree" class="black-icon"/>
-        <div class="detailed-weather">
-            <p>Precipitación: ${current.cloud}%</p>
-            <p>Humedad: ${current.humidity}%</p>
-            <p>Viento: ${current.wind} km/h</p>
-        </div>`
+    <img src="https:${weather.icon}" alt="${weather.text}" />
+    <p>${weather.temp} ºC</p>
+    <img src="../assets/img/degree.png" alt="degree" class="black-icon"/>
+    <div class="detailed-weather">
+        <p>Precipitación: ${weather.cloud}%</p>
+        <p>Humedad: ${weather.humidity}%</p>
+        <p>Viento: ${weather.wind} km/h</p>
+    </div>`
 }
 
 const printHourForecast = (forecast) => {
@@ -44,19 +50,45 @@ const printHourForecast = (forecast) => {
     forecast.forEach(hour => {
         const { time_epoch, condition, temp_c } = hour
         let hourlyPrediction = new Date(time_epoch * 1000).getHours()
-        if (hourlyPrediction < 10) { hourlyPrediction = `0${hourlyPrediction}` }
+
+        hourlyPrediction < 10 ? hourlyPrediction = '0' + hourlyPrediction : hourlyPrediction
 
         const hourLi = document.createElement('li')
         forecastDiv.appendChild(hourLi)
-        
+
         hourLi.innerHTML += `
-                ${hourlyPrediction}:00
-                <img src="https:${condition.icon}" alt="${condition.text}" />
-                ${temp_c} ºC`
+        ${hourlyPrediction}:00
+        <img src="https:${condition.icon}" alt="${condition.text}" />
+        ${temp_c} ºC`
 
         const currentHour = new Date().getHours()
-        if (currentHour == hourlyPrediction) {hourLi.classList.add('now')}
+        if (currentHour == hourlyPrediction) { hourLi.classList.add('now') }
     })
+}
+
+const getLocation = () => {
+    const searchUrl = `${baseUrl}/search.json?key=${apiKey}&q=${searchLocation.value}&aqi=no&lang=es`
+    results.innerHTML = ''
+    selectLocation(searchUrl)
+}
+
+const selectLocation = async (url) => {
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error('Error en la respuesta (location)', response.status)
+        }
+        const data = await response.json()
+        results.classList.remove('hidden')
+        data.forEach(option => {
+            const selectedUrl = `${baseUrl}/forecast.json?key=${apiKey}&q=${option.lat},${option.lon}&aqi=no&lang=es`
+            results.innerHTML += `
+                <li class="select-location" onclick="getWeather('${selectedUrl}')">
+                    ${option.name}, ${option.region}, ${option.country}
+                </li>`
+        })
+    }
+    catch (error) { console.log('Error al obtener los datos', error) }
 }
 
 getWeather(`${baseUrl}/forecast.json?key=${apiKey}&q=${city}&aqi=no&lang=es`)
