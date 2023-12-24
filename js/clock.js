a74916cf6bc86fbe039f907dee20bc83a4368292
María Delgado <mariad1996@gmail.com>
2023-12-24 14:28:21 +0100

    fixed clock, refactored clock and links

--- js/clock.js
+++ js/clock.js
@@ -11,113 +11,107 @@ const currentTimeDate = () => {
         day: '2-digit',
         weekday: 'long'
     })
-
     document.getElementById('time-date').innerHTML = `
         <p class="time">${time}</p>
         <p class="date">${date}</p>`
 
-    printQuote(time)
-    // displayAlarms()
+    verifyQuoteAlarm(time)
+}
+
+const verifyQuoteAlarm = (time) => {
+    let stop = false
+    document.getElementById('quote') ? printQuote(time) : stop = true
+    document.getElementById('alarms-list') ? displayAlarms() : stop = true
 }
 
 const printQuote = (time) => {
-    const quote = document.getElementById('quote')
-
-    if (!quote) { return } else {
-        if (time >= '00:01:00' && time < '07:01:00') {
-            quote.textContent = 'Es hora de dormir, ¡hasta mañana!'
-        } else if (time >= '07:01:00' && time < '12:01:00') {
-            quote.textContent = '¡Buenos días!, desayuna fuerte y a manos a la obra'
-        } else if (time >= '12:01:00' && time < '14:01:00') {
-            quote.textContent = 'Sigue un poco más pero no te olvides de almorzar'
-        } else if (time >= '14:01:00' && time < '16:01:00') {
-            quote.textContent = 'Espero que hayas comido bien, recuerda beber agua'
-        } else if (time >= '16:01:00' && time < '18:01:00') {
-            quote.textContent = 'Buenas tardes, ¡a por el último empujón!'
-        } else if (time >= '18:01:00' && time < '22:01:00') {
-            quote.textContent = 'Esto ya son horas extra, ... piensa en parar pronto'
-        } else if (time >= '22:01:00' && time < '00:01:00') {
-            quote.textContent = 'Buenas noches, ve cerrando y a descansar'
-        }
+    if (time >= '00:01:00' && time < '07:01:00') {
+        quote.textContent = 'Es hora de dormir, ¡hasta mañana!'
+    } else if (time >= '07:01:00' && time < '12:01:00') {
+        quote.textContent = '¡Buenos días!, desayuna fuerte y a manos a la obra'
+    } else if (time >= '12:01:00' && time < '14:01:00') {
+        quote.textContent = 'Sigue un poco más pero no te olvides de almorzar'
+    } else if (time >= '14:01:00' && time < '16:01:00') {
+        quote.textContent = 'Espero que hayas comido bien, recuerda beber agua'
+    } else if (time >= '16:01:00' && time < '18:01:00') {
+        quote.textContent = 'Buenas tardes, ¡a por el último empujón!'
+    } else if (time >= '18:01:00' && time < '22:01:00') {
+        quote.textContent = 'Esto ya son horas extra, ... piensa en parar pronto'
+    } else if (time >= '22:01:00' && time < '00:01:00') {
+        quote.textContent = 'Buenas noches, ve cerrando y a descansar'
     }
 }
 
-// const hourInput = document.getElementById('hour')
-// const minInput = document.getElementById('min')
-// const secInput = document.getElementById('sec')
+const hourInput = document.getElementById('hour')
+const minInput = document.getElementById('min')
+const secInput = document.getElementById('sec')
 
-// const setAlarm = () => {
-//     const date = new Date()
-//     date.setHours(hourInput.value)
-//     date.setMinutes(minInput.value)
-//     date.setSeconds(secInput.value)
+const setAlarm = () => {
+    const date = new Date()
+    date.setHours(hourInput.value)
+    date.setMinutes(minInput.value)
+    date.setSeconds(secInput.value)
 
-//     const formattedAlarm = date.toLocaleTimeString(navigator.language, {
-//         hour: '2-digit',
-//         minute: '2-digit',
-//         second: '2-digit'
-//     })
+    const formattedAlarm = date.toLocaleTimeString(navigator.language, {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    })
 
-//     saveAlarm(formattedAlarm)
-//     displayAlarms()
-//     clearAlarmInput()
-// }
+    saveAlarm(formattedAlarm)
+    clearAlarmInput()
+}
 
-// const saveAlarm = (newAlarm) => {
-//     if (!localStorage.alarms) {
-//         const stringifiedAlarms = JSON.stringify([newAlarm])
-//         localStorage.setItem('alarms', stringifiedAlarms)
-//     } else {
-//         const savedAlarms = JSON.parse(localStorage.alarms)
-//         localStorage.alarms = JSON.stringify([...savedAlarms, newAlarm])
-//     }
-// }
+const saveAlarm = (newAlarm) => {
+    if (!localStorage.alarms) {
+        const stringifiedAlarms = JSON.stringify([newAlarm])
+        localStorage.setItem('alarms', stringifiedAlarms)
+    } else {
+        const savedAlarms = JSON.parse(localStorage.alarms)
+        localStorage.alarms = JSON.stringify([...savedAlarms, newAlarm])
+    }
+}
 
-// const displayAlarms = () => {
-//     if (!document.getElementById('saved-alarms')) { return } else {
-//         const alarmList = document.getElementById('saved-alarms')
-//         alarmList.innerHTML = ''
+const displayAlarms = () => {
+    const alarmList = document.getElementById('alarms-list')
 
-//         if (!localStorage.alarms || ((localStorage.alarms).lenght == 0)) { return } else {
-//             const parsedAlarms = JSON.parse(localStorage.alarms)
-//             const toNumber = x => Number(x.match(/\d+/)[0])
-//             const sortedAlarms = parsedAlarms.sort((a, b) => toNumber(a) - toNumber(b))
+    if (!localStorage.alarms || localStorage.alarms === '[]') {
+        alarmList.classList.remove('alarms-list')
+        alarmList.classList.add('hidden')
+    } else {
+        alarmList.classList.remove('hidden')
+        alarmList.classList.add('alarms-list')
+        alarmList.innerHTML = ''
 
-//             sortedAlarms.forEach(alarm => {
-//                 alarmList.innerHTML += `
-//             <li class="link">
-//                 <p class="saved-alarm">${alarm}</p>
-//                 <button class="delete-link-btn" onclick="deleteAlarm('${alarm}')">x</button>
-//             </li>`
-//             })
-//         }
-//     }
-// }
+        const parsedAlarms = JSON.parse(localStorage.alarms)
+        const toNumber = x => Number(x.match(/\d+/)[0])
+        const sortedAlarms = parsedAlarms.sort((a, b) => toNumber(a) - toNumber(b))
 
-// const deleteAlarm = (alarm) => {
-//     const parsedAlarms = JSON.parse(localStorage.alarms)
+        sortedAlarms.forEach(alarm => {
+            alarmList.innerHTML += `
+            <li class="link">
+                <p class="saved-alarm">${alarm}</p>
+                <button class="delete-link-btn" onclick="deleteAlarm('${alarm}')">x</button>
+            </li>`
+        })
+    }
+}
 
-//     parsedAlarms.filter(element => {
-//         if (element == alarm) {
-//             const index = parsedAlarms.indexOf(alarm)
-//             parsedAlarms.splice(index, 1)
-//             localStorage.alarms = JSON.stringify(parsedAlarms)
-//             displayAlarms()
-//         }
-//     })
-// }
+const deleteAlarm = (alarm) => {
+    const parsedAlarms = JSON.parse(localStorage.alarms)
+    const updatedAlarms = parsedAlarms.filter(element => element !== alarm)
+    localStorage.alarms = JSON.stringify(updatedAlarms)
+    displayAlarms()
+}
 
-// const clearAlarmInput = () => {
-//     hourInput.value = ''
-//     minInput.value = ''
-//     secInput.value = ''
-//     hourInput.focus()
-// }
+const clearAlarmInput = () => {
+    hourInput.value = '00'
+    minInput.value = '00'
+    secInput.value = '00'
+    hourInput.focus()
+}
 
-// const clearStoredAlarms = () => {
-//     localStorage.removeItem('alarms')
-// }
-
+const clearStoredAlarms = () => { localStorage.removeItem('alarms') }
 
 setInterval(currentTimeDate, 1000)
 currentTimeDate()
--- js/links.js
+++ js/links.js
@@ -2,27 +2,30 @@ const savedList = document.getElementById('linkList')
 const linkNameInput = document.getElementById('link-name')
 const linkUrlInput = document.getElementById('link-URL')
 
+const verifyLinks = () => { !localStorage.links ? savedList.innerHTML = '' : printLink() }
+
 const saveLink = () => {
     const linkName = linkNameInput.value
     let linkUrl = linkUrlInput.value
 
-    if (!linkName || !linkUrl) { return }
-    linkUrl.includes('http://') || linkUrl.includes('https://') ? linkUrl : linkUrl = `https://${linkUrl}`
+    if (!linkName || !linkUrl) { return } else {
+        linkUrl.includes('http://') || linkUrl.includes('https://') ? linkUrl : linkUrl = `https://${linkUrl}`
 
-    const newLink = {
-        name: linkName,
-        url: linkUrl
-    }
+        const newLink = {
+            name: linkName,
+            url: linkUrl
+        }
 
-    if (!localStorage.links) {
-        const savedArray = JSON.stringify([newLink])
-        localStorage.setItem('links', savedArray)
-    } else {
-        const oldArray = JSON.parse(localStorage.links)
-        localStorage.links = JSON.stringify([...oldArray, newLink])
+        if (!localStorage.links) {
+            const linksArray = JSON.stringify([newLink])
+            localStorage.setItem('links', linksArray)
+        } else {
+            const storedLinks = JSON.parse(localStorage.links)
+            localStorage.links = JSON.stringify([...storedLinks, newLink])
+        }
+        clearInput()
+        printLink()
     }
-    printLink()
-    clearInput()
 }
 
 const printLink = () => {
@@ -46,26 +49,15 @@ const clearInput = () => {
     linkNameInput.focus()
 }
 
-const clearLinks = () => {
-    localStorage.removeItem('links')
-    savedList.innerHTML = ''
-    document.getElementById('link-name').value = ''
-    document.getElementById('link-URL').value = ''
-}
-
 const deleteLink = (removeName, removeUrl) => {
     const storedLinks = JSON.parse(localStorage.links)
-    storedLinks.filter(link => {
-        if (link.name == removeName && link.url == removeUrl) {
-            const index = storedLinks.indexOf(link)
-            storedLinks.splice(index, 1)
-            localStorage.links = JSON.stringify(storedLinks)
-            printLink()
-        }
-    })
+    const updatedLinks = storedLinks.filter(link => link.name !== removeName && link.url !== removeUrl)
+    localStorage.links = JSON.stringify(updatedLinks)
+    printLink()
 }
 
-!localStorage.links ? savedList.innerHTML = '' : printLink()
+const clearStoredLinks = () => { localStorage.removeItem('links') }
 
+verifyLinks()
 linkNameInput.addEventListener('keydown', (press) => { if (press.key === 'Enter') { saveLink() } })
 linkUrlInput.addEventListener('keydown', (press) => { if (press.key === 'Enter') { saveLink() } })
